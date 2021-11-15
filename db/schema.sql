09d2761b9c1b37b90264fbddae093ac15bbbe848
Benrowen5 <79202800+Benrowen5@users.noreply.github.com>
2021-11-15 09:34:41 -0500

    added to schemas, seeds, and set up functions based on inquirer input

--- db/schema.sql
+++ db/schema.sql
@@ -1,24 +1,29 @@
-DROP DATABASE IF EXISTS employee;
-DROP TABLE IF EXISTS employee;
-DROP TABLE IF EXISTS role;
 DROP TABLE IF EXISTS department;
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS employee;
 
-CREATE DATABASE employee;
+CREATE TABLE department (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL,
+);
+
+CREATE TABLE roles (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
+);
 
 CREATE TABLE employee (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-)
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE SET NULL
+    
+    -- Add for tracking manager of an employee
+    -- CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES role(title)
+    -- CONSTRAINT fk_manager FOREIGN KEY ()
+);
 
-CREATE TABLE role (
-    id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(30) NOT NULL,
-    salary DECIMAL NOT NULL,
-    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
-)
 
-CREATE TABLE department (
-    id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(30) NOT NULL,
-)
+
---
+++ db/seeds.sql
@@ -0,0 +1,25 @@
+INSERT INTO department (name) 
+VALUES
+    ('IT'),
+    ('Engineering'),
+    ('Sales'),
+    ('Marketing');
+
+  INSERT INTO roles (title, salary)
+  VALUES
+    ('Manager', 120000),
+    ('Assistant Manager', '72000'),
+    ('Analyst', 60000),
+    ('Sr. Analyst', 90000)
+    ('Engineer', 70000 ),
+    ('Staff Engineer', 115000);
+
+    INSERT INTO employee (first_name, last_name)
+    VALUES
+    ('Ronald', 'Firbank'),
+    ('Virginia', 'Woolf'),
+    ('Piers', 'Gaveston'),
+    ('Charles', 'LeRoi'),   
+    ('Katherine', 'Mansfield'),
+    ('Dora', 'Carrington'),
+    ('Edward', 'Bellamy');
