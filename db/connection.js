fd708afd9f9b600c82a13b109f30f63caee44c88
Benrowen5 <79202800+Benrowen5@users.noreply.github.com>
2021-11-25 11:33:21 -0500

    added dotenv package for sql user security

--- db/connection.js
+++ db/connection.js
@@ -1,13 +1,14 @@
 const mysql = require('mysql2');
+require('dotenv').config();
 
 // connect to mysql database
 const db = mysql.createConnection(
     {
         // server connection details
         host: 'localhost',
-        user: 'root',
-        password: 'r00t',
-        database: 'employees'
+        user: process.env.DB_USER,
+        password: process.env.DB_PW,
+        database: process.env.DB_NAME
     },
     console.log("Connected to the employees database")
 );
